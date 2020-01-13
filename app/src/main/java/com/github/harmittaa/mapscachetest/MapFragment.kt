fc76ed5f9f477a0a36c02047ee7325b528c7e4b3
Matti Mäki-Kihniä <matti.makikihnigmail.com>
2020-01-13 11:55:54 +0200

    Added side-by-side comparison

--- app/src/main/java/com/github/harmittaa/mapscachetest/MapFragment.kt
+++ app/src/main/java/com/github/harmittaa/mapscachetest/MapFragment.kt
@@ -13,23 +13,36 @@ import com.google.android.gms.maps.model.MarkerOptions
 class MapFragment : Fragment(), OnMapReadyCallback {
 
     private var googleMap: GoogleMap? = null
+    private lateinit var mapView: MapView
     private var mapFragment: SupportMapFragment? = null
-    private var isFirstPass = false
+    private var isFirstPassView = false
+    private var isFirstPassFragment = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        isFirstPass = true
+        isFirstPassView = true
+        isFirstPassFragment = true
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         super.onCreateView(inflater, container, savedInstanceState)
-        return inflater.inflate(R.layout.fragment_maps, container, false)
+        val view = inflater.inflate(R.layout.fragment_maps, container, false)
+        mapView = view.findViewById(R.id.map_view)
+        mapView.onCreate(savedInstanceState)
+
+        return view
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
-        mapFragment?.getMapAsync(this)
+        mapFragment = childFragmentManager.findFragmentById(R.id.map_fragment) as SupportMapFragment
+        mapFragment?.getMapAsync(FragmentMapReadyListener())
+        mapView.getMapAsync(this)
+
         val button: Button = view.findViewById(R.id.button)
         button.setOnClickListener {
             val activity = activity as MapsActivity
@@ -41,13 +54,62 @@ class MapFragment : Fragment(), OnMapReadyCallback {
         this.googleMap = googleMap
 
         // Add a marker in Sydney and move the camera
-        if (isFirstPass) {
-            isFirstPass = false
+        if (isFirstPassView) {
+            isFirstPassView = false
             val sydney = LatLng(-34.0, 151.0)
             googleMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
             googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
 
         }
     }
+
+    inner class FragmentMapReadyListener : OnMapReadyCallback {
+        override fun onMapReady(p0: GoogleMap?) {
+            if (isFirstPassView) {
+                isFirstPassFragment = false
+                val sydney = LatLng(-34.0, 151.0)
+                p0?.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+                p0?.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+
+            }
+        }
+
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        mapView.onSaveInstanceState(outState)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView.onResume()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mapView.onStart()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mapView.onStop()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView.onPause()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mapView.onDestroy()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView.onLowMemory()
+    }
+
 
 }
